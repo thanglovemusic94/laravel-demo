8bf1ad050ec347a76539ed428894a2af5d95e8ca
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-02-24 16:11:50 +0700

    #1 router name

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+
+<ul>
+    <li><a href="/service/1">service 1</a></li>
+    <li><a href="/service/2">service 2</a></li>
+    <li><a href="/service/3">service 3</a></li>
+</ul>
+<ul>
+    <li><a href="{{ route('new', ['id' => 1]) }}">bai viet 1</a></li>
+    <li><a href="{{ route('new', ['id' => 2]) }}">bai viet 2</a></li>
+    <li><a href="/news/3">bai viet 3</a></li>
+</ul>
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test')->name('test');
+});
+
+Route::get('/news/{id}', function ($id){
+    return "bai viet so $id";
+}) ->name('new');
+
+Route::get('/service', function ($id){
+   return "service $id";
 });
